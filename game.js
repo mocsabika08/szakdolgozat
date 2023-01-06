b616545953172b944ff796bc84c58d09586014a4
CodaScapegoat <codascapegoat@protonmail.com>
2023-01-06 17:42:41 +0100

    ball, movement, collision

--- game.js
+++ game.js
@@ -1,15 +1,24 @@
 const cvs = document.getElementById("arkanoid");
 const ctx = cvs.getContext("2d");
 const paddle = {
-    x:190,
-    y:784,
-    width:100,
+    width:150,
     height:20,
+    x:cvs.width/2 - 150/2,
+    y:cvs.height - 20 - 50,
     dx:5
+}
+const ball = {
+    r:10,
+    x:cvs.width/2,
+    y:paddle.y-10,
+    s:3,
+    dx:3,
+    dy:-3
 }
 
 let leftArrow = false;
 let rightArrow = false;
+let lives = 3;
 
 function DrawPaddle(){
     ctx.fillStyle = "black";
@@ -18,18 +27,75 @@ function DrawPaddle(){
     ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
 }
 
-function Update(){
-    if (rightArrow)
+function DrawBall(){
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI*2);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function MovePaddle(){
+    if (rightArrow && paddle.x+paddle.width < cvs.width)
         paddle.x += paddle.dx;
-    if (leftArrow)
+    if (leftArrow && paddle.x > 0)
         paddle.x -= paddle.dx;
 }
 
-function GameLoop(){
+function MoveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+function WallCollision(){
+    if (ball.x+ball.r > cvs.width || ball.x-ball.r < 0){
+        ball.dx = -ball.dx;
+    }
+    if (ball.y-ball.r < 0){
+        ball.dy = -ball.dy;
+    }
+    if (ball.y+ball.r > cvs.height){
+        lives--;
+        ResetBall();
+    }
+}
+
+function PaddleCollision(){
+    if (ball.y>paddle.y && ball.y<paddle.y+paddle.height && ball.x>paddle.x && ball.x<paddle.x+paddle.width){
+        let point = ball.x - (paddle.x+paddle.width/2);
+        point = point / (paddle.width/2);
+        let a = point * (Math.PI/3);
+        ball.dx = ball.s * Math.sin(a);
+        ball.dy = -ball.s * Math.cos(a);
+    }
+}
+
+function ResetBall(){
+    ball.x = cvs.width/2;
+    ball.y = paddle.y-10;
+    ball.dx = 3*(Math.random()*2-1);
+    ball.dy = -3;
+}
+
+function Draw(){
+    DrawBall();
+    DrawPaddle();
+}
+
+function Update(){
+    MovePaddle();
+    WallCollision();
+    PaddleCollision();
+    MoveBall();
+}
+
+function Main(){
     ctx.clearRect(0, 0, cvs.width, cvs.height);
     Update();
-    DrawPaddle();
-    requestAnimationFrame(GameLoop);
+    Draw();
+    requestAnimationFrame(Main);
 }
 
 document.addEventListener("keydown", function(event){
@@ -50,4 +116,4 @@ document.addEventListener("keyup", function(event){
         return;}
 });
 
-GameLoop();
+Main();
