dc6ee153aff0cd5908033132fbab6e23fe9ce376
CodaScapegoat <codascapegoat@protonmail.com>
2023-03-09 21:02:04 +0100

    added login

--- php/main.php
+++ php/main.php
@@ -1,7 +1,41 @@
 <h2>Főmenü</h2>
 
-<p>Üdvözöllek az Arkanoid játékomban!</p>
+<?php
+    if (!empty($_GET['username']) && !empty($_GET['password'])){
+        $cookie_name = "username";
+        $cookie_value = $_GET["username"];
+        setcookie($cookie_name, $cookie_value, time() + 86400, "/");
 
-<p>A játék során egy labdát kell a levegőben tartanod, míg az lerombolja az összes téglát. Két játékmód között választhatsz:</p>
+        $cookie_name = "password";
+        $cookie_value = $_GET["password"];
+        setcookie($cookie_name, $cookie_value, time() + 86400, "/");
 
-<p>A klasszikus módban szintről szintre lépve egyre több téglát kell lerombolni, valamint a labda sebessége is egyre gyorsabb.</p>
+        header("location:./");
+    }
+
+    if (isset($_COOKIE['username'])){
+        print "
+            <p>Üdvözöllek ". $_COOKIE['username'] ." az Arkanoid játékomban!</p>
+            <p>A játék során egy labdát kell a levegőben tartanod, míg az lerombolja az összes téglát. Két játékmód között választhatsz:</p>
+            <p>A klasszikus módban szintről szintre lépve egyre több téglát kell lerombolni, valamint a labda sebessége is egyre gyorsabb.</p>
+        ";
+    }
+    else{
+        print "
+            <form>
+            <form action='index.php'>
+            <table>
+                <tr>
+                    <td><label>Felhasználónév: </label></td>
+                    <td><input type='text' name='username'></td>
+                </tr>
+                <tr>
+                    <td><label>Jelszó: </label></td>
+                    <td><input type='password' name='password'></td>
+                </tr>
+            </table>
+            <button type='submit' name='submit'>Bevitel</button>
+            </form>
+        ";
+    }
+?>
